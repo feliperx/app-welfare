0e7982447bf5a9b1ce3f37bade35cccbb69b8ded
feliperx <feliipe.xaud@gmail.com>
2020-11-19 18:00:19 -0400

    first commit: start project with login screen imcomplete

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  View, 
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native'; 
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  return ( 
+
+    <View style={styles.container}> 
+
+
+          <Image style={styles.header} source={require('./public/images/plus.svg')}/> 
+
+          <TextInput style={styles.input} placeholder="Email"></TextInput>
+          <TextInput style={styles.input} placeholder="Password"></TextInput>
+          
+          <TouchableOpacity
+            style={styles.buttonLogin}
+            onPress={() => alert('Login!')}>
+            <Text style={{ color: '#fafdfb' }}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonSignUp}
+            onPress={() => alert('Sign Up!')}>
+            <Text style={{ color: '#fafdfb' }}>Sign Up</Text>
+          </TouchableOpacity>
+            <Text style={styles.footer}> Forgot your password?</Text>
     </View>
   );
 }
@@ -14,8 +38,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    borderColor: '#fff',
+    backgroundColor: '#fff', 
+    margin: 35,
+  }, 
+  header: { 
+    height: 60, 
+
   },
+  input: {
+    height: 60,
+    marginVertical: 8,
+    paddingHorizontal: 15,
+    borderRadius: 14,
+    borderColor: 'green',
+    backgroundColor: '#f2f2f2',
+    color: '#333', 
+  },
+  buttonLogin: {
+    alignItems: 'center',
+    backgroundColor: '#99E9E6',
+    marginVertical: 8,
+    marginTop: 75,
+    color: '#fafdfb',
+    padding: 15,
+    borderRadius: 14,
+  }, 
+  buttonSignUp: {
+    alignItems: 'center',
+    backgroundColor: '#C5CFDA',
+    marginVertical: 8,
+    color: '#fafdfb',
+    padding: 15, 
+    borderRadius: 14,
+  },  
+  footer: { 
+    marginTop:30,
+    color: '#88FDD0', 
+    textAlign: 'center', 
+    fontWeight: 'bold',
+   },
 });
+
+
