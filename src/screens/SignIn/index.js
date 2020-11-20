d6c6346cbaf9cd06e0e3f1596c7ebe48c5340725
feliperx <feliipe.xaud@gmail.com>
2020-11-20 15:01:36 -0400

    add mainStack.js

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import {Container} from './styles'; 
+import {Text} from 'react-native'; 
+
+import Arrow from '../../../public/images/arrow.svg';
+
+export default () => {
+    return (
+        <Container> 
+            <Text>
+                Sign In
+            </Text> 
+
+        </Container>
+    );
+}
